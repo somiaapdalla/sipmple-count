4054d0207078fb06603730cfc141e3a0aec40a07
somia <somiadeu2003@gmail.com>
2025-07-07 19:52:10 +0300

    in react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+function App() {
+  const[count ,setcount]=useState(0)
+
+  const handleadd=()=>{
+    setcount(count+1)
+  }
+  return (
+    
+    <div className="bg-blue-300 h-screen text-center">
+     <div className="  pt-10 pl-20 pt-64 pb-5 text-white">{count}</div>
+    <button className="bg-blue-500 px-10 py-2 mb-10 text-white  ml-20" onClick={handleadd}>click</button>
+    </div>
+   
+     
+  )
+}
+
+export default App
